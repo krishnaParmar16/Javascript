9ca4309af945b2ef14a9885d9a50d8839939e2e7
krishnaParmar16 <kp497485@gmail.com>
2024-08-12 18:43:20 +0530

    day-15

---
+++ day15/natural.js
@@ -0,0 +1,16 @@
+function sumOfNaturalNumbers(num)
+{
+
+
+    if(num>0)
+        {
+            return num*(num+1)/2;
+        }
+    else
+        {
+            return 0;
+        }    
+
+}
+
+console.log(sumOfNaturalNumbers(5));
